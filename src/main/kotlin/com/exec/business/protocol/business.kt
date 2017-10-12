c8eee157f1d322f1fbe36884280c4da2c61f2d04
VadymPolyanski <vadym.polyanski@gmail.com>
2017-10-12 22:14:31 +0300

    created business api requests and responses

--- src/main/kotlin/com/exec/business/protocol/business.kt
+++ src/main/kotlin/com/exec/business/protocol/business.kt
@@ -3,17 +3,57 @@ package com.exec.business.protocol
 import com.exec.business.protocol.api.Request
 import com.exec.business.protocol.api.Response
 import com.exec.business.protocol.api.RotingData
-import com.exec.business.protocol.dto.BusinessDTO
+import com.exec.business.protocol.dto.BusinessPlanDTO
+import org.codehaus.jackson.annotate.JsonProperty
+import javax.validation.Valid
+import javax.validation.constraints.NotNull
 
 /**
  * Author: Vadym Polyanski;
  * Date: 21.09.17;
  * Time: 8:30.
  */
-data class GetBusinessesRequest (
-        override val rotingData: RotingData?
+data class GetBusinessPlansRequest(
+        override val rotingData: RotingData
 ): Request()
 
-data class GetBusinessesResponse (
-        val businesses: List<BusinessDTO>
+data class GetBusinessPlansResponse(
+        @JsonProperty("business_plans")
+        val businessPlans: List<BusinessPlanDTO>
+): Response()
+
+//create
+data class CreateBusinessPlanRequest (
+        override val rotingData: RotingData,
+        @Valid
+        @NotNull
+        val businessPlan: BusinessPlanDTO
+): Request()
+
+class CreateBusinessPlanResponse : Response()
+
+//update
+data class UpdateBusinessPlanRequest (
+        override val rotingData: RotingData,
+        @Valid
+        @NotNull
+        val businessPlan: BusinessPlanDTO
+): Request()
+
+class UpdateBusinessPlanResponse : Response()
+
+//delete
+class DeleteBusinessPlanRequest(
+        override val rotingData: RotingData?
+) : Request()
+
+class DeleteeBusinessPlanResponse : Response()
+
+//read
+data class GetBusinessPlanRequest (
+        override val rotingData: RotingData
+): Request()
+
+data class GetBusinessPlanResponse(
+        val businessPlan: BusinessPlanDTO
 ): Response()
