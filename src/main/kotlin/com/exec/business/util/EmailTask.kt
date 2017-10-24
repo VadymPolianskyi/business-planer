8aad4407493820b7978137d86e6ef9158e3a6339
VadymPolyanski <vadym.polyanski@gmail.com>
2017-10-24 07:37:01 +0300

    created email execution service for sending message about a deadline

---
+++ src/main/kotlin/com/exec/business/util/EmailTask.kt
@@ -0,0 +1,27 @@
+package com.exec.business.util
+
+import com.exec.business.dao.entity.QuestionEntity
+import com.exec.business.dao.entity.UserEntity
+import java.io.Serializable
+
+
+/**
+ * Author: Vadym Polyanski;
+ * Date: 22.10.17;
+ * Time: 23:45.
+ */
+class EmailTask(private val emailService: EmailService, private val questionEntity: QuestionEntity,
+                val user: UserEntity) : Runnable {
+
+
+    override fun run() {
+        if (user.email != null) {
+            val data = DeadLineData(questionEntity.type.title,
+                    questionEntity.answer, user.firstName, System.currentTimeMillis())
+            emailService.sendMessage(user.email!!, "dead-line", data)
+        }
+    }
+
+   data class  DeadLineData(val question: String, val answer: String?, val user: String?, val time: Long) : Serializable
+
+}
