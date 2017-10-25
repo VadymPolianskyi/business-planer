d0456bcc0bd7ae9daf3626c501cde58a9facc577
VadymPolyanski <vadym.polyanski@gmail.com>
2017-10-25 17:50:19 +0300

    created page and mvc controller

---
+++ src/main/kotlin/com/exec/business/controller/MvcController.kt
@@ -0,0 +1,19 @@
+package com.exec.business.controller
+
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.RequestMapping
+
+/**
+ * Author: Vadym Polyanski
+ * Date: 25.10.17
+ * Time: 11:42
+ */
+
+@Controller
+@RequestMapping("/business-planer")
+class MvcController {
+    @RequestMapping("/main")
+    fun mainPage(): String {
+        return "index"
+    }
+}
