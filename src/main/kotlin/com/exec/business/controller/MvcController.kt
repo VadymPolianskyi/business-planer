3929a958b1a29f7b62e24ce53d6653434f5abf8e
VadymPolyanski <vadym.polyanski@gmail.com>
2017-10-30 17:07:23 +0200

    created short logic for main page

--- src/main/kotlin/com/exec/business/controller/MvcController.kt
+++ src/main/kotlin/com/exec/business/controller/MvcController.kt
@@ -1,6 +1,15 @@
 package com.exec.business.controller
 
+import com.exec.business.dao.entity.secure.UserCredentials
+import com.exec.business.dao.entity.util.PlanStep
+import com.exec.business.handler.business.GetBusinessPlansHandler
+import com.exec.business.protocol.GetBusinessPlansRequest
+import com.exec.business.protocol.api.RotingData
+import com.exec.business.protocol.dto.BusinessPlanDTO
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.security.core.annotation.AuthenticationPrincipal
 import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.RequestMapping
 
 /**
@@ -13,8 +22,24 @@ import org.springframework.web.bind.annotation.RequestMapping
 @RequestMapping("/business-planer")
 class MvcController {
 
+    @Autowired
+    private lateinit var getBusinessPlansHandler: GetBusinessPlansHandler
+
     @RequestMapping("/main")
-    fun mainPage(): String {
+    fun mainPage(model: Model, @AuthenticationPrincipal credentials: UserCredentials): String {
+        val planSteps = PlanStep.values()
+        val plans: List<BusinessPlanDTO> = getBusinessPlansHandler.handle(GetBusinessPlansRequest(RotingData(credentials))).businessPlans
+
+
+        plans.map{plan -> {
+            if (plan.description!!.length > 100)
+                plan.description = plan.description!!.substring(0, 100)
+            }
+        }
+
+        model.addAttribute("businessPlans", plans)
+        model.addAttribute("planSteps", planSteps)
+
         return "index"
     }
 
